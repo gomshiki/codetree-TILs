1ae513a7074ed1c361bc974b3b51ead68524406c
김준성 <105576721+gomshiki@users.noreply.github.com>
2024-01-29 13:04:49 +0900

    Title: 변수 출력하기 2, Time: 91ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240129/변수 출력하기 2/outputing-variables-2.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        char a = 'C';
+        String b = "!.....!";
+        int c = 3;
+
+        System.out.println(a+b+c);
+    }
+}
