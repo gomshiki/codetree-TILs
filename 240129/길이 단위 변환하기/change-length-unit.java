c49908a6706ff9047c1f4e555918eb794d0ec6e3
김준성 <105576721+gomshiki@users.noreply.github.com>
2024-01-29 13:17:16 +0900

    Title: 길이 단위 변환하기, Time: 97ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240129/길이 단위 변환하기/change-length-unit.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        
+        double ft = 9.2;
+        double mile = 1.3;
+
+        double c = 30.48;
+        double d = 160934;
+
+        System.out.printf("%.1fft = %.1fcm\n", ft, ft*c);
+        System.out.printf("%.1fmi = %.1fcm", mile, mile*d);
+
+
+
+
+    }
+}
