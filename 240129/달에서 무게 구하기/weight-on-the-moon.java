864b9e6a102659acb1e7a00369d7a9b595746fa8
김준성 <105576721+gomshiki@users.noreply.github.com>
2024-01-29 13:09:52 +0900

    Title: 달에서 무게 구하기, Time: 94ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240129/달에서 무게 구하기/weight-on-the-moon.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        
+        double a = 13;
+        double b = 0.165000;
+        double c = a*b;
+
+
+
+        System.out.printf("%.0f * %f = %.6f", a, b, c);
+
+    }
+}
