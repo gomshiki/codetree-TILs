982bc22fca1c86a6a050cbbcc5b17a00c15fe896
김준성 <105576721+gomshiki@users.noreply.github.com>
2024-02-02 13:05:06 +0900

    Title: 10개 입력받기, Time: 130ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240202/10개 입력받기/receive-10-inputs.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int sum = 0;
+        int count = 0;
+
+        for(int i = 0 ; i < 10 ; i++){
+            int a = sc.nextInt();
+            if(a == 0 ) break;
+            count++;
+            sum += a;
+        }
+
+
+        System.out.println(sum + " " + (double) sum/count);
+
+    }
+}
