caf4041c713de39f09ab4b07022e8b914c5d5567
김준성 <105576721+gomshiki@users.noreply.github.com>
2024-02-03 07:48:39 +0900

    Title: 100 도달하기, Time: 131ms, Memory: 10MB, Status: Passed - CodeTree

--- 240203/100 도달하기/reach-100.java
+++ 240203/100 도달하기/reach-100.java
@@ -7,13 +7,13 @@ public class Main {
 
         int n = sc.nextInt();
 
-        int[] arry = new int[10];
+        int[] arry = new int[50];
 
         arry[2] = n;
         arry[1] = 1;
         System.out.printf("%d %d ", arry[1], arry[2]);
 
-        for(int i = 3; i < 10 ; i++){
+        for(int i = 3; i < 50 ; i++){
             arry[i] = arry[i-1] + arry[i-2];
             System.out.printf("%d ", arry[i]);
             if(arry[i] > 100) break;
