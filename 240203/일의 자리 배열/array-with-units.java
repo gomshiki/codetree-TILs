2eb83fd8623414fe662cfc40ef4e3058dc6911bc
김준성 <105576721+gomshiki@users.noreply.github.com>
2024-02-03 07:10:42 +0900

    Title: 일의 자리 배열, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240203/일의 자리 배열/array-with-units.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        
+        int[] arry= new int[11];
+
+        arry[1]= sc.nextInt();
+        arry[2]= sc.nextInt();
+
+        for(int i = 3; i <= 11 ; i++){
+            arry[i] = (arry[i-1] + arry[i-2])%10;
+        }
+
+        for(int j = 1; j <= 11 ; j++){
+            System.out.printf("%d " , arry[j]);
+        }
+
+    }
+}
