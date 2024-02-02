173d5470fa767f7fc7511cc9021994e843835115
김준성 <105576721+gomshiki@users.noreply.github.com>
2024-02-03 07:47:42 +0900

    Title: 100 도달하기, Time: 124ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240203/100 도달하기/reach-100.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int[] arry = new int[10];
+
+        arry[2] = n;
+        arry[1] = 1;
+        System.out.printf("%d %d ", arry[1], arry[2]);
+
+        for(int i = 3; i < 10 ; i++){
+            arry[i] = arry[i-1] + arry[i-2];
+            System.out.printf("%d ", arry[i]);
+            if(arry[i] > 100) break;
+        }
+    }
+}
