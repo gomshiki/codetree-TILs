cc1ca70e1ff7d993a815fd06907efce1b194b782
김준성 <105576721+gomshiki@users.noreply.github.com>
2024-02-03 22:00:41 +0900

    Title: 개수 세기, Time: 128ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240203/개수 세기/count-numbers.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int m = sc.nextInt();
+
+        int cnt = 0;
+        for(int i = 0 ; i < n ; i++){
+            int targetNum = sc.nextInt();
+
+            if(targetNum == m) cnt++;
+        }
+
+        System.out.println(cnt);
+    }
+}
