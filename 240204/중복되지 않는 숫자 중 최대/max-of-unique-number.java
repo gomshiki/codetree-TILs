ee49531bc91295aae8ba7fb4dd9d5d9bbeffc237
김준성 <105576721+gomshiki@users.noreply.github.com>
2024-02-04 18:21:46 +0900

    Title: 중복되지 않는 숫자 중 최대, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240204/중복되지 않는 숫자 중 최대/max-of-unique-number.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int[] arry = new int[N];
+        int[] arryIdx = new int[1000];
+
+        // 배열 초기화
+        for(int i = 0; i < N; i++){
+            arryIdx[i] = sc.nextInt();
+
+            // 해당 인덱스 값 count
+            arry[arryIdx[i]]++;
+
+        }
+
+        int maxVal = -1;
+        for(int j = 0; j < N ; j++){
+
+            if(arry[arryIdx[j]] == 1){
+                if(arry[arryIdx[j]] > maxVal){
+                    maxVal = arryIdx[j];
+                }
+            }
+
+        }
+
+        System.out.println(maxVal);
+
+
+
+
+    }
+}
