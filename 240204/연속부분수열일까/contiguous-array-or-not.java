4df99fa09c07b50bf8602cd00e72d919d878b40d
김준성 <105576721+gomshiki@users.noreply.github.com>
2024-02-04 14:35:42 +0900

    Title: 연속부분수열일까, Time: 123ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240204/연속부분수열일까/contiguous-array-or-not.java
+++ 240204/연속부분수열일까/contiguous-array-or-not.java
@@ -37,7 +37,7 @@ public class Main {
                     if(seqA[i] == seqB[j]) cnt++;
                 }
                 result = cnt == seqB.length ? "Yes" : "No";
-                break;
+                continue;
             }else{
                 result = "No";
             }
