fe7ee19df2d2477f38af5377ddee6d10e119a334
김준성 <105576721+gomshiki@users.noreply.github.com>
2024-02-04 16:33:21 +0900

    Title: 가장 큰 수 고르기, Time: 182ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240204/가장 큰 수 고르기/picking-biggest-number.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int[] arry = new int[10];
+
+        int maxVal = 0;
+        for(int i = 0; i < 10; i++){
+            int a = sc.nextInt();
+            if(a > maxVal) maxVal = a;
+        }
+
+        System.out.println(maxVal);
+
+
+
+    }
+}
