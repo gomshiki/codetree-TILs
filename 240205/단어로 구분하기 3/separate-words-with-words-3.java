83920c9f7393da9744e6371be1fb3e3ae9878195
김준성 <105576721+gomshiki@users.noreply.github.com>
2024-02-05 10:32:29 +0900

    Title: 단어로 구분하기 3, Time: 139ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240205/단어로 구분하기 3/separate-words-with-words-3.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        String[] arry = new String[10];
+
+        for(int i = 0 ; i < 10 ; i++){
+            arry[i] = sc.next();
+        }
+
+        for(int j = 9 ; j >= 0 ; j--){
+            System.out.println(arry[j]);
+        }
+    }
+}
