6c3d7534f118a9a144a6a6940229ad49cc5f183d
김준성 <105576721+gomshiki@users.noreply.github.com>
2024-02-06 22:51:37 +0900

    Title: a로 채워넣기, Time: 227ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240206/a로 채워넣기/filling-with-a.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+
+        Scanner sc = new Scanner(System.in);
+
+        String a = sc.next();
+        char[] arr = a.toCharArray();
+
+        arr[1] = 'a';
+        arr[arr.length-2] = 'a';
+
+        String b = String.valueOf(arr);
+
+        System.out.println(b);
+
+    }
+}
