d4d1372a292785acce6804cafcffe0f1091877d1
김준성 <105576721+gomshiki@users.noreply.github.com>
2024-02-07 08:39:22 +0900

    Title: 문자 제거하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240207/문자 제거하기/del-char.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+
+        Scanner sc = new Scanner(System.in);
+
+        String a = sc.next();
+
+        while(a.length() > 1){
+            
+            int b = sc.nextInt();
+
+            if( b > a.length()){
+                a = a.substring(0, a.length()-1);
+            }else{
+                a = a.substring(0, b) + a.substring(b+1);
+            }
+            System.out.println(a);
+        }
+
+        
+    }
+}
