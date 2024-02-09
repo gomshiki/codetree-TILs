03199d43b97a3191e26a96ebe4c3f49f0b3ab833
김준성 <105576721+gomshiki@users.noreply.github.com>
2024-02-09 20:21:29 +0900

    Title: 정수만 추출하기, Time: 125ms, Memory: 10MB, Status: Passed - CodeTree

--- 240209/정수만 추출하기/extract-only-integers.java
+++ 240209/정수만 추출하기/extract-only-integers.java
@@ -1,7 +1,8 @@
 import java.util.Scanner;
-import java.lang.StringBuilder;
 
 public class Main {
+
+    // check below code snippet for main method
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
@@ -12,35 +13,35 @@ public class Main {
 
         int sum = 0;
 
-        while(true){
+        StringBuilder sb = new StringBuilder();
 
-            StringBuilder sb = new StringBuilder();
-            for(int i = 0 ; i < A.length() ; i++){
+        for(int i = 0 ; i < A.length() ; i++){
 
-                char target = A.charAt(i);
-
-                if(target < '0' || target > '9'){
-                    sum += Integer.parseInt(sb.toString());
-                    break;
-                }else{
-                    sb.append(target);
-                }
+            char target = A.charAt(i);
+            if(target < '0' || target > '9'){
+                break;
+            }else{
+                sb.append(target);
             }
-
-            sb = new StringBuilder();
-            for(int i = 0 ; i < B.length() ; i++){
-                char target = B.charAt(i);
-                if(target < '0' || target > '9'){
-                    sum += Integer.parseInt(sb.toString());
-                    break;
-                }else{
-                    sb.append(target);
-                }
-            }
-
-            System.out.println(sum);
-            break;
         }
+        String parseA = sb.toString();
+
+        sum += Integer.parseInt(parseA);
+
+        sb = new StringBuilder();
+        for(int i = 0 ; i < B.length() ; i++){
+            char target = B.charAt(i);
+            if(target < '0' || target > '9'){
+                break;
+            }else{
+                sb.append(target);
+
+            }
+        }
+          String parseB = sb.toString();
+        sum += Integer.parseInt(parseB);
+
+        System.out.println(sum);
 
 
 
