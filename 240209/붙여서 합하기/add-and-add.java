182fba89b3d7bdaad8bd2df730984ad35ec80518
김준성 <105576721+gomshiki@users.noreply.github.com>
2024-02-09 19:29:57 +0900

    Title: 붙여서 합하기, Time: 161ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240209/붙여서 합하기/add-and-add.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+
+        Scanner sc = new Scanner(System.in);
+
+        String A = sc.next();
+
+        String B = sc.next();
+
+        int AB = Integer.parseInt(A+B);
+        int BA = Integer.parseInt(B+A);
+
+        System.out.println(AB+BA);
+    }
+}
