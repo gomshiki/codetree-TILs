bc46236fa5c7dad0cc1147c5af6f0c047a27b772
김준성 <105576721+gomshiki@users.noreply.github.com>
2024-02-13 17:38:32 +0900

    Title: 짝수이면서 합이 5의 배수인 수, Time: 148ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240213/짝수이면서 합이 5의 배수인 수/an-even-number-with-a-multiple-of-5-in-the-sum.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        func(n);
+        
+    }
+
+    static void func(int n){
+
+        if(n % 2 ==0){
+
+            int a = n % 10; // 1의 자리
+            int b = (n / 10); // 10의 자리 
+
+            if((a+b) % 5 == 0){
+                System.out.println("Yes");
+            }else{
+                System.out.println("No");
+            }
+
+        }
+
+    }
+
+}
