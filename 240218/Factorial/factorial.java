d397a434e194684c73cf206982b4b4aed4d26706
김준성 <105576721+gomshiki@users.noreply.github.com>
2024-02-18 11:44:39 +0900

    Title: Factorial, Time: 139ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240218/Factorial/factorial.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int resutlt = func(n);
+
+        System.out.println(resutlt);
+
+    }
+
+    static int func( int n ){ // f(5)
+        if(n == 0) return 1;
+
+        return func(n - 1) * n;
+    }
+}
