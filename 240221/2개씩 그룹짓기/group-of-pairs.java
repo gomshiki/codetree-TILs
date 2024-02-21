413175e90f6a7aaf6d586ba03431fb83dd49065d
김준성 <105576721+gomshiki@users.noreply.github.com>
2024-02-21 16:45:42 +0900

    Title: 2개씩 그룹짓기, Time: 127ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240221/2개씩 그룹짓기/group-of-pairs.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc= new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr= new int[n];
+
+        for(int i = 0 ; i < n ; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        Arrays.sort(arr);
+
+        int result = func(arr);
+
+        System.out.println(result);
+    }
+
+    static int func(int[] arr){
+
+        int max = 0;
+
+        for(int i = 0 ; i < arr.length; i++){
+
+            int temp = arr[i] + arr[arr.length - i - 1];
+
+            if(temp > max) max = temp;
+
+        }
+
+        return max;
+
+    }
+}
