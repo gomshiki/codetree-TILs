ff9f3dba86170b7b5b37720a0603af3688855fed
김준성 <105576721+gomshiki@users.noreply.github.com>
2024-02-23 08:51:57 +0900

    Title: Date to Date, Time: 139ms, Memory: 10MB, Status: Passed - CodeTree

--- 240223/Date to Date/date-to-date.java
+++ 240223/Date to Date/date-to-date.java
@@ -1,37 +1,31 @@
 import java.util.Scanner;
 
 public class Main {
+    public static int numOfDays(int m, int d) {
+        // 계산 편의를 위해 월마다 몇 일이 있는지를 적어줍니다. 
+        int[] days = new int[]{0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        int totalDays = 0;
+        
+        // 1월부터 (m - 1)월 까지는 전부 꽉 채워져 있습니다.
+        for(int i = 1; i < m; i++)
+            totalDays += days[i];
+        
+        // m월의 경우에는 정확이 d일만 있습니다.
+        totalDays += d;
+        
+        return totalDays;
+    }
+
     public static void main(String[] args) {
-        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
 
-        Scanner sc =new Scanner(System.in);
-
+        // 변수 선언 및 입력:
         int m1 = sc.nextInt();
         int d1 = sc.nextInt();
         int m2 = sc.nextInt();
         int d2 = sc.nextInt();
-
-        // 월별 일수 Array                1   2   3   4   5   6   7   8   9   10  11  12
-        int[] num_of_days = new int[]{0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-
-        // 경과일
-        int elapsedDays = 1;
-
-        while(true){
-
-            if(m1 == m2 && d1 == d2){
-                System.out.println(elapsedDays);
-                break;
-            }
-            elapsedDays++;
-            d1++;
-            
-
-            if(d1 == num_of_days[m1]){
-                m1++;
-                d1 = 1;
-            }
-
-        }
+        
+        // 출력
+        System.out.println(numOfDays(m2, d2) - numOfDays(m1, d1) + 1);
     }
 }
