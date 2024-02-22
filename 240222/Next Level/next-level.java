f8e5f446fb939ab12f944887a991b0d4a71e6b0d
김준성 <105576721+gomshiki@users.noreply.github.com>
2024-02-22 09:34:48 +0900

    Title: Next Level, Time: 147ms, Memory: 10MB, Status: Passed - CodeTree

--- 240222/Next Level/next-level.java
+++ 240222/Next Level/next-level.java
@@ -8,8 +8,8 @@ class User{
     }
 
     public User(){
-        this.id = "codetree";
-        this.level = "10";
+        this.id = "";
+        this.level = "";
     }
 
 }
@@ -23,6 +23,10 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         User user1 = new User();
+
+        user1.id = "codetree";
+        user1.level = "10";
+
         User user2 = new User(sc.next(), sc.next());
 
         System.out.printf("user %s lv %s\n", user1.id, user1.level);
