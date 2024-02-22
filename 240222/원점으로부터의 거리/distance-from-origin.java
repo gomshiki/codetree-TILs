77e6e42c670f6359bf13212e43cdf39f30e70421
김준성 <105576721+gomshiki@users.noreply.github.com>
2024-02-22 13:04:32 +0900

    Title: 원점으로부터의 거리, Time: 124ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240222/원점으로부터의 거리/distance-from-origin.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+class Point implements Comparable<Point> {
+    int x,y, id;
+
+    public Point(int x , int y, int id){
+        this.x = x;
+        this.y = y;
+        this.id = id;
+    }
+
+    @Override
+    public int compareTo(Point point){
+        return Math.abs(this.x - point.x) + Math.abs(this.y- point.y);
+    }
+
+
+}
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        Point[] points = new Point[n];
+
+        for(int i = 0 ; i < n ; i++){
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            points[i] = new Point(x, y, i+1);
+        }
+
+        Arrays.sort(points);
+
+        for(int i = 0 ; i < n ; i++){
+            System.out.println(points[i].id);
+        }
+
+    }
+}
