83be0863f5d5e15fce5ca06188ae031aa391f565
김준성 <105576721+gomshiki@users.noreply.github.com>
2024-03-13 13:13:57 +0900

    Title: 연속되는 수 4, Time: 115ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240313/연속되는 수 4/continuous-number4.java
+++ 240313/연속되는 수 4/continuous-number4.java
@@ -13,7 +13,7 @@ public class Main {
             arr[i] = sc.nextInt();
         }
 
-        int cnt = 0 , ans = 0;
+        int cnt = 1 , ans = 0;
 
         for(int i = 0 ; i < arr.length ; i++){
 
